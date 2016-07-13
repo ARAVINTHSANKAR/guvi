12645b005504e262c4ea24ab22bc24dfdb1110be
ARAVINTHSANKAR <aravinthsankareee@gmail.com>
2016-07-13 17:11:31 +0530

    Add files via upload

---
+++ realnum.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<string.h>
+#include<conio.h>
+
+int main()
+{
+int a[10];
+int b[10];
+int c;
+printf("enter the array");
+for(i=o;i<=10;i++)
+{
+scanf("%d",a[i]);
+}
+b[10]=a[10];
+for(i=0;i<=10;i++)
+{
+for(j=0;j<=i;j++)
+{
+if(a[i]!==b[j])
+{
+c=a[i];
+}
+}}
+printf("%d",c);
+return 0;
+}
+}
+
+
+
+
