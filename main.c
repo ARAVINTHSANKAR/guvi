20404888552ef532cd3d59334c0323f1f5e0973c
ARAVINTHSANKAR <aravinthsankareee@gmail.com>
2016-07-13 12:04:29 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,13 @@
+
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+	char a[20],char b[20];
+	printf("enter the string");
+    scan("%s",&a);
+	char b[20]=strrev(a);
+	printf(string is"%s" ,b);
+return 0;
+}
