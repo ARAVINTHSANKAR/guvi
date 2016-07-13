d9f5fe20c1f94339a706261c5ed2600a1cc875b7
ARAVINTHSANKAR <aravinthsankareee@gmail.com>
2016-07-13 17:53:38 +0530

    Add files via upload

---
+++ original.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a[100];
+int x,i;
+printf("enter the elements");
+for(i=0;i<=100;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<=100;i++);
+{
+x=x^a[i]
+}
+printf("%d",&x);
+return 0;
+}
