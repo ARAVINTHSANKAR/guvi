256b9bf1504d3249b14862c2d4e91c0818364560
ARAVINTHSANKAR <aravinthsankareee@gmail.com>
2016-07-19 17:41:33 +0530

    Add files via upload

---
+++ positive.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int num;
+printf("enter the num");
+scanf("%d",&num);
+if(num=0)
+{
+printf("idiot");
+}
+if(num<0)
+{
+printf("its negative");
+}
+else
+printf(" its positive");
+return 0;
+}
