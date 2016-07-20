d5b78e75db224a69a15a6d96e371177382a25a03
ARAVINTHSANKAR <aravinthsankareee@gmail.com>
2016-07-20 12:22:28 +0530

    Add files via upload

--- fact.c
+++ fact.c
@@ -1,14 +1,17 @@
-#include<stdio.h>
-int main()
-{
-int fact=1,i,num;
-printf("enter the num");
-scanf("%d",&num)
+#include <stdio.h>
 
-for(i=0;i<=num;i++);
+void main()
 {
-fact=fact*i;
-}
-printf(factorial is:"%d",fact);
-return 0;
+    int num,fact=1;
+    int i;
+  printf("enter the  num");
+    scanf("%d",&num);
+    if((num==0)||(num==1))
+    {
+        return 1;
+    }
+    for(i=1;i<num;i++)
+    {
+        fact=fact*i;
+    }
 }
