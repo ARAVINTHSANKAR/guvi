8196df2e617d43b962beb2b0f9dbb5ca4660564f
ARAVINTHSANKAR <aravinthsankareee@gmail.com>
2016-09-28 21:40:23 +0530

    Add files via upload

---
+++ hunter 33 (1).c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+int num;
+scanf("%d",&num);
+switch(num)
+{
+    case 2 : printf("ABC");
+    break;
+     case 3 : printf("DEF");
+      break;
+     case 4 : printf("GHI"); 
+      break;
+      case 5 : printf("JKL");
+      break;
+      case 6 : printf("MNO");
+      break;  
+      case 7 : printf("PQRS"); 
+      break;  
+      case 8 : printf("TUV");
+        break;
+        case 9 : printf("XYZ");
+         break;
+       default : printf("  ");
+}
+}
+
